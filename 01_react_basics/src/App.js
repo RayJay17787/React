09f8527d1dcea8ed778a26414fb8b7b7b8508e9b
RayJay17787 <rayyanme174@gmail.com>
2026-04-08 12:03:59 -0700

    Started React

---
+++ 01_react_basics/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          <code>Edit src/App.js and save to reload.</code>
+        </p>
+        <a
+          className="App-link"
+          href="https://facebook.com"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          This is my first React App
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
