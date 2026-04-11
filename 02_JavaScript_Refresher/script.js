1809f6c045ad48b1d938a8f921158cefd21d82ae
RayJay17787 <rayyanme174@gmail.com>
2026-04-11 11:48:28 -0700

    Changes in script.js

--- 02_JavaScript_Refresher/script.js
+++ 02_JavaScript_Refresher/script.js
@@ -14,3 +14,60 @@
 // }
 // myFunc(48)
 
+// Callbacks in JavaScript
+
+// setTimeout(() => {
+//    console.log("This is a callback function which runs after 3s") 
+// }, 3000);
+
+// let myFunc = () => {
+//     console.log("This is an arrow function")
+// }
+// myFunc()
+
+// Promises in JavaScript
+
+// Syntax of Promises in JavaScript looks like:
+/*
+    let promise = new Promise((resolve, reject) => {
+        *Code Execution*
+    });
+*/
+
+// let promise = new Promise((resolve, reject) => {
+//     console.log("Hello *")
+//     resolve(56)
+// })
+
+// console.log("Hello 1")
+
+// setTimeout(() => {console.log("Hello 2")}, 2000)
+
+// function myFunc(){
+//     return "Hello 3"
+// }
+// console.log(myFunc())
+// console.log(promise)
+
+
+let promise1 = new Promise((resolve, reject) => {
+    console.log("Promise is pending right now")
+    setTimeout(() => {
+        resolve(true)
+    }, 5000)
+})
+
+let promise2 = new Promise((resolve, reject) => {
+    console.log("Promise is pending right now")
+    setTimeout(() => {
+        reject(new Error("I am an error"))
+    }, 5000)
+})
+
+promise1.then(() => {
+    console.log("I got accepted")
+})
+
+promise2.catch((error) => {
+    console.log("Some error has occurred. Please reload the page!")
+})
