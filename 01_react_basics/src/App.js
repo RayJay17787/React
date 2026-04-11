7550f8094c3f014bf4d7b34284ea1397499c7f67
RayJay17787 <rayyanme174@gmail.com>
2026-04-11 08:00:38 -0700

    Changes

--- 01_react_basics/src/App.js
+++ 01_react_basics/src/App.js
@@ -1,12 +1,22 @@
 import logo from './logo.svg';
 import './App.css';
 
+function Button(){
+  return(
+  <div>
+  <h4><center>Button Below</center></h4>
+  <button>This is a button</button>
+  </div>
+  )
+}
+
 function App() {
+  let myName = "npm Rayyan"
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
+        {/* <p>
           <code>Edit src/App.js and save to reload.</code>
         </p>
         <a
@@ -16,7 +26,9 @@ function App() {
           rel="noopener noreferrer"
         >
           This is my first React App
-        </a>
+        </a> */}
+        <h1>Hello {myName}, Welcome To React!</h1>
+        <Button />
       </header>
     </div>
   );
---
+++ 02_JavaScript_Refresher/script.js
@@ -0,0 +1,15 @@
+"use strict";
+
+// const arr = [2, 3, 4, 5]
+
+// // console.log(arr.length)
+
+// let i;
+// for (i = 0; i < arr.length; i++){
+//     console.log(arr[i])
+// }
+
+// let myFunc = (number) => {
+//     console.log(`This is a number: ${number}`)
+// }
+// myFunc(48)
