625fc2a457c27f646c391259dbb48a5bac4481b5
RayJay17787 <rayyanme174@gmail.com>
2026-04-12 04:17:07 -0700

    Async Await

--- 02_JavaScript_Refresher/script.js
+++ 02_JavaScript_Refresher/script.js
@@ -139,3 +139,151 @@
 // promise2.catch((error) => {
 //     alert("An unexpected error has occurred")
 // })
+
+// Promise Chaining
+
+// let p1 = new Promise((resolve, reject) => {
+//     console.log("Resolving...")
+//     setTimeout(() => {
+//         console.log("Resolved after 5 seconds")
+//         resolve(27)
+//     }, 5000)
+// })
+
+// p1.then((value) => {
+//     console.log(value)
+
+//     let p2 = new Promise((resolve, reject) => {
+//         console.log("Promise 2 initializing...")
+//         setTimeout(() => {
+//             resolve("Hello!")
+//         }, 2000)
+//     })
+//     return p2
+// }).then((value) => {
+//     console.log(value)
+
+//     let p3 = new Promise((resolve, reject) => {
+//         console.log("Promise 3 initializing...")
+//         setTimeout(() => {
+//             resolve("How are you?")
+//         }, 4000)
+//     })
+//     return p3
+// }).then((value) => {
+//     console.log(value)
+
+//     let p4 = new Promise((resolve, reject) => {
+//         console.log("Promise 4 initializing...")
+//         setTimeout(() => {
+//             resolve("Where are you from?")
+//         }, 5000)
+//     })
+//     return p4
+// }).then((value) => {
+//     console.log(value)
+
+//     let p5 = new Promise((resolve, reject) => {
+//         console.log("Promise 5 initializing...")
+//         setTimeout(() => {
+//             resolve("Nice to meet you!")
+//         }, 6000)
+//     })
+//     return p5
+// }).then((value) => {
+//     console.log(value)
+// })
+
+// let p1 = new Promise((resolve, reject) => {
+//     console.log("P1 initializing...")
+//     setTimeout(() => {
+//         resolve("Hello!")
+//     }, 5000)
+//     return p1
+// }).then((value) => {
+//   console.log(value)  
+
+//   let p2 = new Promise((resolve, reject) => {
+//     console.log("P2 initializing...")
+//     setTimeout(() => {
+//         resolve("How are you?")
+//     }, 5000)
+//   })
+//   return p2
+// }).then((value) => {
+//     console.log(value)
+
+//     let p3 = new Promise((resolve, reject) => {
+//         console.log("P3 initializing...")
+//         setTimeout(() => {
+//             resolve("What's your name?")
+//         }, 5000)
+//     })
+//     return p3
+// }).then((value) => {
+//     console.log(value)
+
+//     let p4 = new Promise((resolve, reject) => {
+//         console.log("P4 initializing...")
+//         setTimeout(() => {
+//             resolve("What's your age?")
+//         }, 5000)
+//     })
+//     return p4
+// }).then((value) => {
+//     console.log(value)
+
+//     let p5 = new Promise((resolve, reject) => {
+//         console.log("P5 initializing...")
+//         setTimeout(() => {
+//             resolve("Nice to meet you!")
+//         }, 5000)
+//     })
+//     return p5
+// }).then((value) => {
+//     console.log(value)
+
+//     let p6 = new Promise((resolve, reject) => {
+//         console.log("P6 initializing...")
+//         setTimeout(() => {
+//             resolve("Code will now Terminate.")
+//         }, 5000)
+//     })
+//     return p6
+// }).then((value) => {
+//     console.log(value)
+
+//     let p7 = new Promise((resolve, reject) => {
+//         console.log("P7 initializing...")
+//         setTimeout(() => {
+//             resolve("Code Terminated")
+//         }, 5000)
+//     })
+//     return p7
+// }).then((value) => {
+//     alert(value)
+// })
+
+// Async/Await in JavaScript
+// Async functions always return a promise. Any function can be made into an Async function and return a promise.
+
+myFunc = async () => {
+    return "This is an Asynchronous Function"
+}
+
+myFunc().then((value) => {
+    alert(value)
+})
+
+
+
+
+
+
+
+
+
+
+
+
+
