7353f65420c15ba5c5e6d37c00c3fb4af3e12533
RayJay17787 <rayyanme174@gmail.com>
2026-04-16 14:27:25 +0500

    Changes

--- 03_project_1_react_bootstrap/src/App.js
+++ 03_project_1_react_bootstrap/src/App.js
@@ -2,16 +2,11 @@ import './App.css';
 import Navbar from './Components/Navbar'
 import TypeFace_logo from './Components/TypeFace_logo'
 
-let TermObj = {
-  home: "Home",
-  about: `About Us`,
-  contact: "Contact Us"
-}
 
 function App() {
   return (
     <>
-    <Navbar title = {<TypeFace_logo />} navOption1 = {TermObj.home} navOption2 = {<>About <TypeFace_logo /></>} navOption3 = {TermObj.contact}/>
+    <Navbar title = {<TypeFace_logo />} navOption1 = {"Home"} navOption2 = {<>About <TypeFace_logo/></>} navOption3 = {"Contact Us"}/>
     </>
   );
 }
--- 03_project_1_react_bootstrap/src/Components/Navbar.js
+++ 03_project_1_react_bootstrap/src/Components/Navbar.js
@@ -1,28 +1,32 @@
 import React from 'react'
+import PropTypes from 'prop-types'
 
-export default function Navbar(props) {
+export default function Navbar({
+    title = "Set Title Here",
+    navOption1 = "Navbar Option",
+    navOption2 = "Navbar Option",
+    navOption3 = "Navbar Option"
+}) {
   return (
     <div>
         <nav className="navbar navbar-expand-lg navbar-dark bg-primary">
             <div className="container-fluid">
-                <a className="navbar-brand px-3" href="/">{props.title}</a>
+                <a className="navbar-brand px-3 text-white" href="/">{title}</a>
                 <button className="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
                     <span className="navbar-toggler-icon"></span>
                 </button>
                 <div className="collapse navbar-collapse" id="navbarSupportedContent">
-                    <div className="ms-auto">
-                        <ul className="navbar-nav gap-1 me-auto ms-auto px-4 mb-2 mb-lg-0">
+                        <ul className="navbar-nav ms-auto gap-2 px-4 mb-2 mb-lg-0">
                             <li className="nav-item">
-                                <a className="nav-link active" aria-current="page" href="/">{props.navOption1}</a>
+                                <a className="nav-link active" aria-current="page" href="/">{navOption1}</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link px-3" href="/">{props.navOption2}</a>
+                                <a className="nav-link px-3" href="/">{navOption2}</a>
                             </li>
                             <li className="nav-item">
-                                <a className="nav-link" href="/">{props.navOption3}</a>
+                                <a className="nav-link" href="/">{navOption3}</a>
                             </li>
                         </ul>
-                    </div>
                     <form className="d-flex" role="search">
                         <input className="form-control me-2" type="search" placeholder="Search" aria-label="Search" />
                         <button className="btn btn-primary text-white" type="submit"><b>Search</b></button>
